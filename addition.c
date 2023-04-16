34dbe08021362c7659d369c3c872522188048f3d
rkhelif <raphi031@hotmail.fr>
2023-04-16 12:14:10 +0200

    addition finnished

--- addition.c
+++ addition.c
@@ -12,6 +12,17 @@ size_t  ft_strlen(char *str){
     return index;
 }
 
+size_t  ft_strlenAP(char *str, size_t *ap){
+    size_t  i = 0;
+    while (str[i] != '\0' && str[i] != '.')
+        i++;
+    while (str[i] != '\0'){
+        *ap = *ap + 1;
+        i++;
+    }
+    return i;
+}
+
 int     checkIfNumber(char *number){
     int     point = 0;
     size_t  index = 0;
@@ -93,12 +104,121 @@ char *  cleanNumber(char *num){
     return newNum;
 }
 
-char *  addition(char *num1, char *num2){
-    if (num1 == NULL || num2 == NULL)
+char * ft_joinCharBefore(char *str, char c){
+    char *num = NULL;
+    size_t  len = 0;
+
+    if (str == NULL){
+        num = (char *)malloc(sizeof(char) * 2);
+        if (num == NULL)
+            return NULL;
+        num[0] = c;
+        num[1] = '\0';
+        return num;
+    }
+    len = ft_strlen(str);
+    num = (char *)(malloc(sizeof(char) * (len + 2)));
+    if (num == NULL){
         return NULL;
+    }
+    for (size_t i = 0; i < len + 1; i++){
+        if (i == 0)
+            num[i] = c;
+        else
+            num[i] = str[i - 1];
+    }
+    num[len + 1] = '\0';
+    return num;
+}
+
+char *  addClean(char *nb1, char *nb2){
+    char *str = NULL;  
+    char *num = NULL;
+    char *cleanNum = NULL;
+    size_t  lenNb1 = 0;
+    size_t  lenNb2 = 0;
+    size_t  ap1 = 0;
+    size_t  ap2 = 0;
+    char    c = 0;
+    char    val = 0;
+
+    lenNb1 = ft_strlenAP(nb1, &ap1);
+    lenNb2 = ft_strlenAP(nb2, &ap2);
+    // printf("lenNb1 = %ld\n", lenNb1);
+    // printf("lenNb2 = %ld\n", lenNb2);
+    // printf("ap1 = %ld\n", ap1);
+    // printf("ap2 = %ld\n", ap2);
+    while (ap1 > 0 || ap2 > 0){
+        if (ap1 > ap2){
+            c = (nb1[lenNb1 - 1]);
+            lenNb1--;
+            ap1--;
+        }
+        else if (ap2 > ap1){
+            c = (nb2[lenNb2 - 1]);
+            lenNb2--;
+            ap2--;
+        }
+        else if (ap1 == ap2){
+            if (ap1 != 1){
+                c = ((nb1[lenNb1 - 1] - 48) + (nb2[lenNb2 - 1] - 48) + val);
+                val = c / 10;
+                c = (c % 10) + 48;
+            }else{
+                c = '.';
+            }
+            ap1--;
+            ap2--;
+            lenNb1--;
+            lenNb2--;
+        }
+        str = num;
+        num = ft_joinCharBefore(str, c);
+        if (str != NULL){
+            free(str);
+            str = NULL;
+        }
+        if (num == NULL)
+            return NULL;
+    }
+    while (lenNb1 > 0 || lenNb2 > 0){
+        if (lenNb1 > 0 && lenNb2 > 0){
+            c = ((nb1[lenNb1 - 1] - 48) + (nb2[lenNb2 - 1] - 48) + val);
+            val = c / 10;
+            c = (c % 10) + 48;
+        }else if (lenNb1 > 0){
+            c = ((nb1[lenNb1 - 1] - 48) + val);
+            val = c / 10;
+            c = (c % 10) + 48;
+        }else if (lenNb2 > 0){
+            c = ((nb2[lenNb2 - 1] - 48) + val);
+            val = c / 10;
+            c = (c % 10) + 48;
+        }
+        lenNb1--;
+        lenNb2--;
+        str = num;
+        num = ft_joinCharBefore(str, c);
+        if (str != NULL){
+            free(str);
+            str = NULL;
+        }
+        if (num == NULL)
+            return NULL;
+    }
+    cleanNum = cleanNumber(num);
+    free(num);
+    num = NULL;
+    return cleanNum;
+}   
+
+char *  addition(char *num1, char *num2){
     char *nb1clean = NULL;
     char *nb2clean = NULL;
+    char *numberAdd = NULL;
 
+    if (num1 == NULL || num2 == NULL)
+        return NULL;
     if (checkIfNumber(num1) == 0 || checkIfNumber(num2) == 0){
         printf("error\n");
         return NULL;
@@ -112,8 +232,16 @@ char *  addition(char *num1, char *num2){
         nb1clean = NULL;
         return NULL;
     }
-    printf("nb1clean = %s\n", nb1clean);
-    printf("nb2clean = %s\n", nb2clean);
+    // printf("nb1clean = %s\n", nb1clean);
+    // printf("nb2clean = %s\n", nb2clean);
+    numberAdd = addClean(nb1clean, nb2clean);
+    if (numberAdd == NULL)
+        return NULL;
 
-    return nb1clean;
+    free(nb1clean);
+    nb1clean = NULL;
+    free(nb2clean);
+    nb2clean = NULL;
+    // printf("numberAdd = %s\n", numberAdd);
+    return numberAdd;
 }
